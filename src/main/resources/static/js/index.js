88469701841bef5c2768ae3754658a08acac1f91
温芳盛 <wenfs@yonyou.com>
2019-04-30 10:31:58 +0800

    完成管理页面（初稿）

---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,22 @@
+layui.use(["element", "jquery"], function() {
+  var element = layui.element,
+    $ = layui.jquery;
+
+  // $(".left-open i").click(function(event) {
+  //   if ($(".layui-side").css("width") == "0px") {
+  //     $(".layui-side").animate(
+  //       {
+  //         width: "200px"
+  //       },
+  //       100
+  //     );
+  //   } else {
+  //     $(".layui-side").animate(
+  //       {
+  //         width: "0px"
+  //       },
+  //       100
+  //     );
+  //   }
+  // });
+});
